017fd61e110b9bb61862a8c27b6b8e39795e612a
Sergey Vernigora <115031024+sergey-vernyk@users.noreply.github.com>
2026-03-22 19:45:50 +0200

    Update settings

--- project/core/settings.py
+++ project/core/settings.py
@@ -29,11 +29,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", True)
+DEBUG = os.environ.get("DEBUG", "True") == "True"
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost").split(",")
 
-STATIC_ROOT = "static"
+STATIC_ROOT = BASE_DIR / "static"
 
 # Application definition
 
